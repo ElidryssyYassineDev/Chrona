8d1c71b1d092fc02e55cc7ac56a89abdb90d5aae
ELIDRYSSYyassine <kings4breakfast69@gmail.com>
2026-08-19 21:08:29 +0100

    feat(api): add JWT bearer authentication and protect employees/me endpoint

--- src/Chrona.Api/Program.cs
+++ src/Chrona.Api/Program.cs
@@ -1,8 +1,24 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = "http://localhost:8080/realms/chrona";
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidAudience = "React-SPA"
+        };
+    } );
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -14,6 +30,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGet("/api/v1/employees/me", () => "Authenticated!").RequireAuthorization();
 app.MapGet("/health", () =>
 {
     return "healthy";
